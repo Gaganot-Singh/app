ccc92b0310a0f6f897a2c994ff08939c3579bdcc
Gaganot-Singh <133317799+Gaganot-Singh@users.noreply.github.com>
2024-03-21 10:48:08 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,7 @@ const projects = [
       'Unique layout with fixed header on left-hand side and movable right portion',
     ],
     image: "./portfolio.png",
+    link: "https://github.com/Gaganot-Singh/app",
     skills: ["Next.js", "Tailwind CSS", "AOS Library", "Responsive Design", "UI/UX Design"]
     },
     {
